035b33b3930531c0cf6b42ad9ce152ad70333c2f
roman <roman.seko@gmail.com>
2013-09-21 14:00:38 +0200

    poriadky v moduloch

--- modules/importerosc/translations/sk.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-
-$_MODULE['<{importerosc}prestashop>importerosc_9aa1b03934893d7134a660af4204f2a9'] = 'Server';
-$_MODULE['<{importerosc}prestashop>importerosc_dc647eb65e6711e155375218212b3964'] = 'Heslo';
